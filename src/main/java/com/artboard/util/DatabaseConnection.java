d3d693d269a8557efefcf052e1de6525c27dc8ee
Amir Khubeev <amir.hubeev@yandex.ru>
2025-10-05 13:40:34 +0300

    database connection

---
+++ src/main/java/com/artboard/util/DatabaseConnection.java
@@ -0,0 +1,19 @@
+package com.artboard.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/artboard";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "7777";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
